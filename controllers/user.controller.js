24dbc0e429894f94541c2a54c47e7554035846de
Alma Alonso <alma.aa93@gmail.com>
2018-07-31 20:32:47 +0200

    wipi wipi wiiiip

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const User = require('../model/user.model');
 
 module.exports.profile = (req, res, next) => {
-    console.log(req)
+    console.log(req.user.gallery)
     let id = req.user._id
     User.findById(id)
     .then(user => {
@@ -18,3 +18,37 @@ module.exports.profile = (req, res, next) => {
     })
     
 }
+
+// module.exports.edit = (req, res, next) => {
+//     const id = req.params.id;
+
+//     User.findById(id)
+//         .then(user => {
+//             if(user) {
+//                 res.render('partials/form', { user })
+//             } else { error }
+//         })
+//         .catch(error => {
+//             next(error);
+//         })
+// }
+
+// module.exports.doEdit = (req, res, next) => {
+//     const id = req.params.id;
+
+//     User.findById()
+//         .then(user => {
+//             if (user){
+//                 Object.assign(user, req.body);
+//                 user.save()
+//                     .then(()=> {
+//                         res.redirect(`/users/${id}`)
+//                     })
+//                     .catch(error => {next(error);})
+//             } else {
+//                 next(error);
+//             }
+            
+//         })
+//         .catch(error => { next(error);})
+// }
