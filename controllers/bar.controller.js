6de21b7ba7b4fcaa5eddde807dc0f9491726b6dd
Belen <>
2018-07-31 21:34:36 +0200

    bar details and update

--- controllers/bar.controller.js
+++ controllers/bar.controller.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const createError = require('http-errors');
 const Bar = require('../model/bar.model');
 
 module.exports.list = (req, res, next) => {
     Bar.find()
-    .then(bars => {
-        res.render('partials/bars/bar-list', {
-            bars
+        .then(bars => {
+            res.render('partials/bars/bar-list', {
+                bars
+            })
         })
-    .catch(error => {
-        next(error)
+        .catch(error => next(error));
+}
+
+module.exports.detail = (req, res, next) => {
+    const id = req.params.id;
+
+    Bar.findById(id)
+        .then(bar => {
+            res.render('partials/bars/bar', { bar })
         })
-    })
+        .catch(error => next(error));
+}
+
+module.exports.edit = (req, res, next) => {
+    const id = req.params.id;
+
+    Bar.findById(id)
+        .then(bar => {
+            if (bar) {
+                res.render('partials/form', { bar })
+            } else {
+                next(createError(404));
+            }
+        })
+        .catch(error => next(error));
+}
+
+module.exports.doEdit = (req, res, next) => {
+    const id = req.params.id;
+
+    const updateSet = {
+        description: req.body.description
+    }
+
+    Bar.findByIdAndUpdate(id, { $set: updateSet }, { new: true, runValidators: true })
+        .then(bar => {
+            if (bar) {
+                res.redirect(`/bars/${id}`)
+            } else {
+                next(createError(404));
+            }
+        })
+        .catch(error => next(error));
 }
--- routes/bar.router.js
+++ routes/bar.router.js
@@ -4,4 +4,8 @@ const barController = require('../controllers/bar.controller');
 
 router.get('/', barController.list);
 
+router.get('/:id', barController.detail);
+router.get('/:id/edit', barController.edit);
+router.post('/:id/edit', barController.doEdit);
+
 module.exports = router;
