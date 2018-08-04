5ae2ddf8649370c82aa4e82d25c1521a2c5197c5
Belen <>
2018-08-04 16:35:02 +0200

    location con lat y long sin marker

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
-        }
-    );
-});
+window.onload = () => {
+    let latitude = Number($('#latitude').val());
+    let longitude = Number($('#longitude').val());
+
+    let bar = {
+        lat: latitude, 
+        lng: longitude
+    };
+    console.log(bar);
+
+    const map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: bar
+        });
+}
+
