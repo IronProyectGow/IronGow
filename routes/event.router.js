624275b7bd48f65f3a73fd9dfbf0f0500d83bcc6
Belen <>
2018-08-05 17:03:51 +0200

    update eventos

--- routes/event.router.js
+++ routes/event.router.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/event.controller');
 
-//outer.get('/createEvent', eventController.createBarEvent);
 router.post('/createEvent', eventController.doCreateBarEvent);
 
 router.get('/:id', eventController.detail);
+router.get('/:id/edit', eventController.edit);
+router.post('/:id/edit', eventController.doEdit);
 
 module.exports = router;
