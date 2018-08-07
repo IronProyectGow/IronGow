a0ec1793f5480bbe09637df1f05d90d13c4ae593
Belen <>
2018-08-07 21:33:29 +0200

    maps update con calle

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,73 +1,52 @@
-// window.onload = () => {
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//         center: {
-//             lat: 40.43, 
-//             lng: -3.69
-//         },
-//         zoom: 13
-//     });
-
-//     // var destinoElegido = $('#pac-input').val()
-
-//     var autocomplete = new google.maps.places.Autocomplete(input);
-
-//     autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
-
-//     autocomplete.addListener('place_changed', function() {
-//         // infowindow.close();
-//         marker.setVisible(false);
-//         var place = autocomplete.getPlace();
-//         console.log(place);
-//         $('#adressInput').val(place);
-
-//         if (!place.geometry) {
-//           // User entered the name of a Place that was not suggested and
-//           // pressed the Enter key, or the Place Details request failed.
-//           window.alert("No details available for input: '" + place.name + "'");
-//           return;
-//         }
-    
-//         // If the place has a geometry, then present it on a map.
-//         if (place.geometry.viewport) {
-//           map.fitBounds(place.geometry.viewport);
-//         } else {
-//           map.setCenter(place.geometry.location);
-//           map.setZoom(17);  // Why 17? Because it looks good.
-//         }
-//         marker.setPosition(place.geometry.location);
-//         marker.setVisible(true);
-    
-//         var address = '';
-//         if (place.address_components) {
-//           address = [
-//             (place.address_components[0] && place.address_components[0].short_name || ''),
-//             (place.address_components[1] && place.address_components[1].short_name || ''),
-//             (place.address_components[2] && place.address_components[2].short_name || '')
-//           ].join(' ');
-//         }
-    
-//         // infowindowContent.children['place-icon'].src = place.icon;
-//         // infowindowContent.children['place-name'].textContent = place.name;
-//         // infowindowContent.children['place-address'].textContent = address;
-//         // infowindow.open(map, marker);
-//       });
-// }
-
-
 window.onload = () => {
-    let latitude = Number($('#latitude').val());
-    let longitude = Number($('#longitude').val());
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.43, 
+            lng: -3.69
+        },
+        zoom: 13
+    });
 
-    let bar = {
-        lat: latitude, 
-        lng: longitude
-    };
+    const input = document.getElementById("pac-input");
 
-    const map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 15,
-            center: bar
-        });
+    var autocomplete = new google.maps.places.Autocomplete(
+      input
+    );
+
+    autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);  // Why 17? Because it looks good.
+        }
+
+        document.getElementById("latitude").value = map.getCenter().lat();
+        document.getElementById("longitude").value = map.getCenter().lng();
+      });
+
+ }
+
+
+// window.onload = () => {
+//     let latitude = Number($('#latitude').val());
+//     let longitude = Number($('#longitude').val());
+
+//     let bar = {
+//         lat: latitude, 
+//         lng: longitude
+//     };
+
+//     const map = new google.maps.Map(
+//         document.getElementById('map'), {
+//             zoom: 15,
+//             center: bar
+//         });
     
-    const marker = new google.maps.Marker({position: bar, map: map});
-}
+//     const marker = new google.maps.Marker({position: bar, map: map});
+// }
