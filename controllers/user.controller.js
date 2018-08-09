46f0d0b5676eb735dd7b7d5efc4fa1010f3676b0
Alma Alonso <alma.aa93@gmail.com>
2018-08-09 19:38:11 +0200

    roles wip

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,17 +3,44 @@ const createError = require('http-errors');
 const constants = require('../constants');
 const mongoose = require('mongoose');
 const User = require('../model/user.model');
+const Artist = require('../model/artist.model');
+const Bar = require('../model/bar.model');
+
 
 module.exports.profile = (req, res, next) => {
-    let id = req.user._id
+    let id = req.user._id;
+    const user = req.user;
+
+    
+    console.log('ENTRO', user);
+
     User.findById(id)
     .then(user => {
-        res.render('partials/users/user', {
-            user
-        })
+        console.log(user)
+
+        if(user.role === 'fan') {
+            res.render('partials/users/user', {
+                user
+            })
+        } else if (user.role === 'artist'){
+            // Query de Mongoose
+            
+
+            res.render('partials/artists/artist', {
+                artist,
+                events
+            })
+        } else {
+            res.render('partials/bars/bar', {
+                bar
+            })
+        }
+        
     })
     .catch(error => {
         next(error)
     })
     
 }
+
+
