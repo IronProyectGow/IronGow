8c225bc405b9ba0487df2393c25148da9bb3bff3
Alma Alonso <alma.aa93@gmail.com>
2018-08-10 13:04:04 +0200

    middlewares added

--- routes/artist.router.js
+++ routes/artist.router.js
@@ -1,12 +1,14 @@
+const constants = require('../constants');
 const express = require('express');
 const router = express.Router();
 const artistController = require('../controllers/artist.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.get('/', artistController.list);
 
 router.get('/:id', artistController.detail);
-router.get('/:id/edit', artistController.edit);
+router.get('/:id/edit', authMiddleware.checkRole(constants.ROLE_ARTIST), artistController.edit);
 router.post('/:id/edit', artistController.doEdit);
 
 module.exports = router;
