362ae9d0bd4aa796e0758e432b3ac6a9f368a370
Belen <>
2018-08-11 11:27:47 +0200

    done

--- helpers/misc.helpers.js
+++ helpers/misc.helpers.js
@@ -7,14 +7,13 @@ module.exports = (hbs) => {
         return ''
     }
   });
-}
 
-    hbs.registerHelper('isSelf', function(array, options) {
-      if(array[0] == array[1]){
-        return options.fn(this);
-      } else {
-        return options.inverse(this);
-      }
-    })
+  hbs.registerHelper('isSelf', function(array, options) {
+    if(array[0] == array[1]){
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  })
 }              
 
